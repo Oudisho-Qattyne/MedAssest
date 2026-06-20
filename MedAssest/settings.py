03840cafb6cc8041dfaa380736ec2952e7dc7d14
stelianchurch <stelianweek@gmail.com>
2026-06-20 17:38:59 +0300

    fixed CORS again

--- MedAssest/settings.py
+++ MedAssest/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = ["*"]
 # ==========================================
 # CORS & INTEGRATION
 # ==========================================
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 # ==========================================
 # APPLICATION DEFINITION
