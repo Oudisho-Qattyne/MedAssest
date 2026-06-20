4c4d9b0184dbfc2a83338d6ee725b45d17fe6b04
stelianchurch <stelianweek@gmail.com>
2026-06-20 17:23:57 +0300

    fixed CORS

--- MedAssest/settings.py
+++ MedAssest/settings.py
@@ -29,12 +29,7 @@ ALLOWED_HOSTS = ["*"]
 # ==========================================
 # CORS & INTEGRATION
 # ==========================================
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5174",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://charity-fund-frontend.vercel.app",
-]
+CORS_ALLOWED_ORIGINS = ["*"]
 
 # ==========================================
 # APPLICATION DEFINITION
